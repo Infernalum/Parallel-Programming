15e9f694994a58e408cc227b409f6f90cc849527
Infernal <vladiusmc@mail.ru>
2021-10-03 22:59:28 +0300

    upd1

---
+++ lab1/lab1.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <time.h>
+
+int main(int argc, char** argv) {
+	const int count = 1e8;
+	const int threads = 16;
+	int random_seed = 920215;
+	const int expr = 10;
+	int  max;
+	double start, end;
+
+	printf("count of threads: %d\n", threads);
+
+	for (int k = 0; k < expr; ++k) {
+		int* array = NULL;
+		srand(random_seed);
+		array = (int*)malloc(count * sizeof(int));
+		for (int i = 0; i < count; i++) { array[i] = (rand() * rand()); }
+		printf("seed: %d:\n", random_seed);
+		max = array[0];
+		start = omp_get_wtime();
+		for (int i = 0; i < count; ++i) {
+			if (array[i] > max) { max = array[i]; };
+		};
+		end = omp_get_wtime();
+		printf("Max element for consecutive function: %d, time: %f;\n", max, end - start);
+
+		double time = 0, all_time = 0;
+
+		for (int j = 0; j < expr; ++j) {
+			max = array[0];
+			time = 0;
+			int i;
+			start = omp_get_wtime();
+#pragma omp parallel num_threads(threads) shared(array, count) reduction(max: max) default(none)
+			{
+#pragma omp for
+				for (i = 0; i < count; i++)
+				{
+					if (array[i] > max) { max = array[i]; };
+				}
+				//printf("%d\n", omp_get_max_threads());
+			}
+			end = omp_get_wtime();
+
+			time = end - start;
+			//printf("======\nMax element for parallel function: %d, time: %f;\n", max, time);
+			all_time += time;
+		}
+		printf("Max element for parallel function:    %d, time: %f;\n", max, all_time / (double)expr);
+		random_seed += 10;
+		max = array[0];
+		free(array);
+	}
+	return(0);
+}
