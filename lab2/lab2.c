306fd2e057dab17faf49fc4aec3c9b2a9cc14804
Infernal <vladiusmc@mail.ru>
2021-10-07 21:10:26 +0300

    lab2

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <omp.h>
+#include <time.h>
+#include <omp.h>k
 
 int main(int argc, char **argv)
 {
-    const int threads = 2;
+    const int threads = 6;
     int seed = 9154821;
     const long count = 1e8;
     const int accuracy = 10;
-    const double frequency = 1;
+    const double frequency = 3;
     const int massives = 100;
     int target = 1;
-    int index = -1;
-    double start = 0, end = 0, time = 0;
+    volatile int index = -1;
+    double start = 0, end = 0, _time = 0;
+    double consecutive_time = 0, parallel_time = 0;
     double average_time = 0;
     double current_frequency = 0;
 
@@ -21,7 +23,7 @@ int main(int argc, char **argv)
     int *array = 0;
     for (int i = 0; i < massives; ++i)
     {
-        //printf("seed: %d;\n", seed);
+        printf("seed: %d;\n", seed);
         srand(seed);
         current_frequency = (rand() % (int)(frequency * 10 + 1 - 0) + 0) / 10.0;
         array = (int *)malloc(count * sizeof(int));
@@ -37,9 +39,11 @@ int main(int argc, char **argv)
                     break;
                 }
             end = omp_get_wtime();
-            time += (end - start);
+            _time += (end - start);
         }
-        time /= (double)accuracy;
+        _time /= (double)accuracy;
+        consecutive_time += _time;
+
         if (index == -1)
             ++empty;
         else if (index < count / 3)
@@ -48,25 +52,12 @@ int main(int argc, char **argv)
             ++middle;
         else
             ++right;
-        printf(" Found occurence of %d at index %d, \ttime: %.8f;\n", target, index, time);
-        free(array);
-        seed += 10;
-        index = -1;
-        average_time += time;
-        time = 0;
-    }
-    printf(" Statistics by the location of the found element:\n empty - %d; left - %d; middle - %d; right - %d;\n", empty, left, middle, right);
-    printf(" Average searching time (consecutive algorithm): \t%.8f\n", average_time / (double)massives);
+        printf(" Found occurence of %d at index %d;\n", target, index);
+        printf(" Average searching time (consecutive algorithm): \t%.8f\n", _time);
 
-    seed -= (accuracy * massives);
-    time = average_time = 0;
-    for (int i = 0; i < massives; ++i)
-    {
-        srand(seed);
-        current_frequency = (rand() % (int)(frequency * 10 + 1 - 0) + 0) / 10.0;
-        array = (int *)malloc(count * sizeof(int));
-        for (int i = 0; i < count; i++)
-            array[i] = rand() % (int)(count / frequency + 1 - 0) + 0;
+        _time = 0;
+        index = -1;
+
         for (int j = 0; j < accuracy; ++j)
         {
             volatile int flag = 0;
@@ -77,8 +68,8 @@ int main(int argc, char **argv)
 #pragma omp for
                 for (p = 0; p < threads; p++)
                 {
-                    for (int j = p * (count / threads); j < (p + 1) * (count / threads); ++j)
-                    //for (int j = p; j < count; j = j + threads)
+                    //for (int j = p * (count / threads); j < (p + 1) * (count / threads); ++j)
+                    for (int j = p; j < count; j = j + threads)
                     {
                         if (flag == 1)
                             break;
@@ -86,20 +77,24 @@ int main(int argc, char **argv)
                         {
                             index = j;
                             flag = 1;
-                            break;
                         }
                     }
                 }
             }
 
             end = omp_get_wtime();
-            time += (end - start);
+            _time += (end - start);
         }
-        seed += 10;
+        _time /= (double)accuracy;
+        parallel_time += _time;
+        printf(" Found occurence of %d at index %d;\n", target, index);
+        printf(" Average searching time (parallel algorithm): \t\t%.8f\n", _time);
+        seed += 1000;
         free(array);
     }
-    time /= (double)accuracy;
-    printf(" Average searching time (parallel algorithm): \t\t%.8f\n", time / (double)massives);
 
+    printf(" Statistics by the location of the found element:\n empty - %d; left - %d; middle - %d; right - %d;\n", empty, left, middle, right);
+    printf(" Average searching time (consecutive algorithm): \t%.8f\n", consecutive_time / (double)massives);
+    printf(" Average searching time (parallel algorithm): \t\t%.8f\n", parallel_time / (double)massives);
     return (0);
 }
