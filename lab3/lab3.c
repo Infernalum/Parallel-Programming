c598100e015e3b3106d0c6f6e7666b13a8afd38f
Infernal <vladiusmc@mail.ru>
2021-10-21 00:42:04 +0300

    lab3

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -3,25 +3,48 @@
 #include <time.h>
 #include <omp.h>
 
-int ifSort(int *array, int size)
+#define _THREADS 5
+#define _SIZE 1e7
+#define _ARRAYS 1
+#define _FREQUENCY 0.1
+
+int comp(const void *elem1, const void *elem2)
 {
-    for (int i = 0; i < size - 1; ++i)
+    int f = *((int *)elem1);
+    int s = *((int *)elem2);
+    if (f > s)
+        return 1;
+    if (f < s)
+        return -1;
+    return 0;
+}
+
+int isEquals(int *array1, int *array2)
+{
+    for (int i = 0; i < _SIZE; ++i)
+        if (array1[i] != array2[i])
+            return 0;
+    return 1;
+}
+
+int isSort(int *array)
+{
+    for (int i = 0; i < _SIZE - 1; ++i)
         if (array[i] > array[i + 1])
             return 0;
     return 1;
 }
 
-void InsertionSort(int *array, int size)
+int *InsertionSort(int *array)
 {
-    int *object = (int *)malloc(sizeof(int) * size);
-    for (int i = 0; i < size; ++i)
+    int *object = (int *)malloc(sizeof(int) * _SIZE);
+    for (int i = 0; i < _SIZE; ++i)
         object[i] = array[i];
     double start, end;
-    int iter = 0;
 
     /* Sorting */
     start = omp_get_wtime();
-    for (int i = 1; i < size; ++i)
+    for (int i = 1; i < _SIZE; ++i)
     {
         int key = object[i];
         int j = i - 1;
@@ -29,144 +52,150 @@ void InsertionSort(int *array, int size)
         {
             object[j + 1] = object[j];
             --j;
-            ++iter;
         }
         object[j + 1] = key;
     }
     end = omp_get_wtime();
     /* Show sorted array */
-    //for (int i = 0; i < size; ++i)
-    //    printf("%d, ", object   [i]);
-    printf("Insertion Sort time: \t\t%.8f, totally iterations: \t%d;\n", end - start, iter);
-    printf("%d\n", ifSort(object, size));
-    free(object);
+    //for (int i = 0; i < _SIZE; ++i)
+    //    printf("%d, ", object[i]);
+    //printf("\n");
+    printf("Insertion Sort time: \t\t%.8f;\n", end - start);
+    printf("Is it sorted? - \t%d;\n", isSort(object));
+    return object;
 }
 
-void ShellSort(int *array, int size)
+int *ShellSort(int *array)
 {
-    int *object = (int *)malloc(sizeof(int) * size);
-    for (int i = 0; i < size; ++i)
+    int *object = (int *)malloc(sizeof(int) * _SIZE);
+    for (int i = 0; i < _SIZE; ++i)
         object[i] = array[i];
     double start, end;
-    int iter = 0;
     start = omp_get_wtime();
-    int tmp;
 
-    for (int step = size / 2; step > 0; step /= 2)
+    for (int step = _SIZE / 2; step > 0; step /= 2)
     {
-        for (int i = 0; i < size; ++i)
+        for (int i = 0; i < step; ++i)
         {
-            for (int j = i + step; j < size; j += step)
+            for (int j = i + step; j < _SIZE; j += step)
             {
-                tmp = object[i];
-                if (tmp > object[j])
+                int key = object[j], p = j - step;
                 {
-                    object[i] = object[j];
-                    object[j] = tmp;
-                    ++iter;
-                    //printf("Поток 0 меняет местами элементы %d и %d;\n", i, j);
+                    while (p >= 0 && object[p] > key)
+                    {
+                        object[p + step] = object[p];
+                        p -= step;
+                    }
+                    object[p + step] = key;
                 }
             }
         }
+
+        //for (int i = 0; i < _SIZE; ++i)
+        //    printf("%d, ", object[i]);
+        //printf("\n");
     }
     end = omp_get_wtime();
     /* Show sorted array */
     //for (int i = 0; i < size; ++i)
     //    printf("%d, ", object[i]);
     //printf("\n");
-    printf("Shell Sort time: \t\t%.8f, totally itera tions: \t%d;\n", end - start, iter);
-    printf("%d\n", ifSort(object, size));
-    free(object);
+    printf("Shell Sort time: \t\t%.8f;\n", end - start);
+    printf("Is it sorted? - \t%d;\n", isSort(object));
+    return object;
 }
 
-void ShellParallelSort(int *array, int size, int threads)
+int *ShellParallelSort(int *array)
 {
-    int *object = (int *)malloc(sizeof(int) * size);
-    for (int i = 0; i < size; ++i)
+    int *object = (int *)malloc(sizeof(int) * _SIZE);
+    for (int i = 0; i < _SIZE; ++i)
         object[i] = array[i];
     double start, end;
     start = omp_get_wtime();
 
-#pragma omp parallel num_threads(threads) shared(object), firstprivate(size)
+omp_set_num_threads(_THREADS);
+#pragma omp parallel shared(object)
     {
-        int m = omp_get_thread_num();
-        for (int step = size / 2; step > 0; step /= 2)
+        for (int step = _SIZE / 2; step > 0; step /= 2)
         {
-            int N = (size - step) / threads;
-#pragma omp parallel for ordered
-            for (int i = 0; i < threads; ++i)
+#pragma omp for
+            for (int i = 0; i < step; ++i)
             {
-                for (int p = i * N; p < (i + 1) * N; ++p)
+                //printf("step: %d; thread: %d; i: %d;\n", step, omp_get_thread_num(), i);
+                for (int j = i + step; j < _SIZE; j += step)
                 {
-#pragma omp ordered
+                    int key = object[j], p = j - step;
                     {
-                        int j = p;
-                        while (j >= 0 && object[j] > object[j + step])
+                        while (p >= 0 && object[p] > key)
                         {
-                            int tmp = object[j];
-                            object[j] = object[j + step];
-                            object[j + step] = tmp;
-                            //printf("Поток %d меняет местами элементы %d и %d;\n", m, j, j + step);
-                            --j;
+                            object[p + step] = object[p];
+                            p -= step;
                         }
+                        object[p + step] = key;
                     }
                 }
             }
+            // #pragma omp single
+            //             {
+            //                 for (int i = 0; i < _SIZE; ++i)
+            //                     printf("%d, ", object[i]);
+            //                 printf("\n");
+            //             }
         }
     }
 
     end = omp_get_wtime();
     /* Show sorted array */
-    //for (int i = 0; i < size; ++i)
+    //for (int i = 0; i < _SIZE; ++i)
     //    printf("%d, ", object[i]);
     //printf("\n");
     printf("Shell Parallel Sort time: \t%.8f;\n", end - start);
-    printf("%d\n", ifSort(object, size));
-    free(object);
+    printf("Is it sorted? - \t%d;\n", isSort(object));
+    return object;
 }
 
 int main(int argc, char **argv)
 {
-    const int threads = 2;
     int seed = 915482;
-    const long count = 1e4;
-    const int accuracy = 10;
-    const double frequency = 0.1;
-    const int massives = 1;
-    volatile int index = -1;
     double start = 0, end = 0, _time = 0;
     double consecutive_time = 0, parallel_time = 0;
     double average_time = 0;
 
     int *array = 0;
-    for (int i = 0; i < massives; ++i)
+    int *pattern = 0;
+    int *cmp1 = 0;
+    int *cmp2 = 0;
+    int *cmp3 = 0;
+    for (int i = 0; i < _ARRAYS; ++i)
     {
         printf("seed: %d;\n", seed);
         srand(seed);
-        array = (int *)malloc(count * sizeof(int));
-        for (int i = 0; i < count; i++)
-            array[i] = rand() % (int)(count / 0.1 + 1 - 0) + 0;
-        //for (int i = 0; i < count; ++i)
-        //     printf("%d, ", array[i]);
-        //   printf("\n");
-        InsertionSort(array, count);
-        ShellSort(array, count);
-        ShellParallelSort(array, count, threads);
-        for (int j = 0; j < accuracy; ++j)
-        {
-        }
+        array = (int *)malloc(_SIZE * sizeof(int));
+        for (int i = 0; i < _SIZE; i++)
+            array[i] = rand() % (int)(_SIZE / _FREQUENCY + 1 - 0) + 0;
+        pattern = (int *)malloc(_SIZE * sizeof(int));
+        for (int i = 0; i < _SIZE; ++i)
+            pattern[i] = array[i];
 
-        index = -1;
-
-        for (int j = 0; j < accuracy; ++j)
-        {
-        }
-
+        double start, end;
+        start = omp_get_wtime();
+        qsort(pattern, _SIZE, sizeof(*pattern), comp);
+        end = omp_get_wtime();
+        //for (int i = 0; i < _SIZE; ++i)
+        //    printf("%d, ", array[i]);
+        //printf("\n");
+        printf("QuickSort time: \t\t%.8f;\nIs it sorted? - %d;\n", end - start, isSort(pattern));
+        //cmp1 = InsertionSort(array);
+        //printf("Are they equal? - \t%d;\n", isEquals(cmp1, pattern));
+        cmp2 = ShellSort(array);
+        printf("Are they equal? - \t%d;\n", isEquals(cmp2, pattern));
+        cmp3 = ShellParallelSort(array);
+        printf("Are they equal? - \t%d;\n", isEquals(cmp3, pattern));
         seed += 1000;
         free(array);
+        free(cmp1);
+        free(cmp2);
+        free(cmp3);
     }
-
-    printf(" Average searching time (consecutive algorithm): \t%.8f\n", consecutive_time / (double)massives);
-    printf(" Average searching time (parallel algorithm): \t\t%.8f\n", parallel_time / (double)massives);
     return (0);
 }
