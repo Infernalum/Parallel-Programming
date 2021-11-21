f6a05f487cfab890f8f29bf6371eaacf3ca6e1b8
Infernal <vladiusmc@mail.ru>
2021-11-21 14:51:25 +0300

    lab5

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -2,22 +2,25 @@
 #include <stdio.h>
 #include <mpi.h>
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-	int ret  = -1;	///< For return values
-	int size = -1;	///< Total number of processors
-	int rank = -1;	///< This processor's number
+	int ret = -1;  ///< For return values
+	int size = -1; ///< Total number of processors
+	int rank = -1; ///< This processor's number
 
-	const int count = 1e1; ///< Number of array elements
+	const int count = 1e1;			///< Number of array elements
 	const int random_seed = 920215; ///< RNG seed
 
-	int* array = 0; ///< The array we need to find the max in
-	int lmax = -1;	///< Local maximums
-	int  max = -1;  ///< The maximal element
+	int *array = 0; ///< The array we need to find the max in
+	int lmax = -1;	///< Local xmaximums
+	int max = -1;	///< The maximal element
 
 	/* Initialize the MPI */
 	ret = MPI_Init(&argc, &argv);
-	if (!rank) { printf("MPI Init returned (%d);\n", ret); }
+	if (!rank)
+	{
+		printf("MPI Init returned (%d);\n", ret);
+	}
 
 	/* Determine our rankand processor count */
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -26,14 +29,18 @@ int main(int argc, char** argv)
 	printf("MPI Comm Rank: %d;\n", rank);
 
 	/* Allocate the array */
-	array = (int*)malloc(count * sizeof(int));
+	array = (int *)malloc(count * sizeof(int));
 
 	/* Master generates the array */
-	if (!rank) {
+	if (!rank)
+	{
 		/* Initialize the RNG */
 		srand(random_seed);
 		/* Generate the random array */
-		for (int i = 0; i < count; i++) { array[i] = rand(); }
+		for (int i = 0; i < count; i++)
+		{
+			array[i] = rand();
+		}
 	}
 
 	//printf("Processor #%d has array: ", rank);
@@ -44,19 +51,25 @@ int main(int argc, char** argv)
 	MPI_Bcast(array, count, MPI_INTEGER, 0, MPI_COMM_WORLD);
 
 	printf("Processor #%d has array: ", rank);
-	for (int i = 0; i < count; i++) { printf("%d ", array[i]); }
+	for (int i = 0; i < count; i++)
+	{
+		printf("%d ", array[i]);
+	}
 	printf("\n");
 
-	const int wstart = (rank    ) * count / size;
-	const int wend   = (rank + 1) * count / size;
+	const int wstart = (rank)*count / size;
+	const int wend = (rank + 1) * count / size;
 
 	printf("Processor #%d checks items %d .. %d;\n", rank, wstart, wend - 1);
 
 	for (int i = wstart;
-		i < wend;
-		i++)
+		 i < wend;
+		 i++)
 	{
-		if (array[i] > lmax) { lmax = array[i]; }
+		if (array[i] > lmax)
+		{
+			lmax = array[i];
+		}
 	}
 
 	printf("Processor #%d reports local max = %d;\n", rank, lmax);
@@ -64,10 +77,11 @@ int main(int argc, char** argv)
 	MPI_Reduce(&lmax, &max, 1, MPI_INTEGER, MPI_MAX, 0, MPI_COMM_WORLD);
 
 	ret = MPI_Finalize();
-	if (!rank) { 
+	if (!rank)
+	{
 		printf("\n*** Global Maximum is %d;\n", max);
 	}
 	printf("MPI Finalize returned (%d);\n", ret);
 
-	return(0);
+	return (0);
 }
